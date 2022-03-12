f629a7778d23aa9f10e71881d17f7f0d45763eba
berggy <88812903+berggy@users.noreply.github.com>
2022-03-12 14:59:50 +0800

    update install file
    The http://dl.alfresco.com/release/community/201707-build-00028 url is dead. so you have to download alfresco-community-installer-201707-linux-x64.bin from https://sourceforge.net/projects/alfresco/files/ and put it in ./assets

--- assets/install_alfresco.sh
+++ assets/install_alfresco.sh
@@ -4,13 +4,15 @@ set -e
 # vars
 export JAVA_HOME=/usr/java/latest
 ALF_HOME=/alfresco
+
+#The following url is dead. so we download from https://sourceforge.net/projects/alfresco/files/
 ALF_BIN=alfresco-community-installer-201707-linux-x64.bin
-ALF_URL=http://dl.alfresco.com/release/community/201707-build-00028/$ALF_BIN
+#ALF_URL=http://dl.alfresco.com/release/community/201707-build-00028/$ALF_BIN
 
 # get alfresco installer
 mkdir -p $ALF_HOME
 cd /tmp
-curl -O $ALF_URL
+#curl -O $ALF_URL
 chmod +x $ALF_BIN
 
 # install alfresco
